2ff33ad7e131880febcf5d70d7c6644709d032b4
Anton Onipko <antono@starsgroup.com>
2021-10-11 21:19:52 +0300

    proto done

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const { Telegraf } = require('telegraf');
 const { Pool } = require('pg');
 const util = require('util');
 
+const WELCOME_MSG = `Вітаю! Я бот, що вміє видобувати відео з TikTok посилань та пересилати їх іншим людям.`;
+const ADMIN_MSG = `Зроби мене адміністратором, щоб я міг бачити усі повідомлення.`;
+const LINK_MSG = `Перешли це повідомлення до чату, до якого надсилатимеш TikTok посилання. Пам'ятай, я маю бути адміністратором у тому чаті, щоб я міг бачити усі повідомлення.\n\n`;
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
@@ -17,16 +21,49 @@ const path = '/' + encodeURIComponent(process.env.BOT_TOKEN);
 bot.telegram.setWebhook('https://share-tiktok-telegram-bot.herokuapp.com' + path);
 bot.startWebhook(path, null, process.env.PORT || 8080);
 
+const pairingChatIds = [];
+const directions = {};
+
 bot.start(ctx => {
   console.log(util.inspect(ctx.update, false, 10));
-  ctx.reply(`Вітаю! Я бот, що вміє видобувати відео з TikTok посилань та пересилати їх іншим людям. Перешли це повідомлення до чату, куди я надсилатиму відео. Пам'ятай, я маю бути адміністратором аби отримувати всі повідомлення.`);
+  var welcome = WELCOME_MSG;
+  if(ctx.update.message.chat.type == 'group')
+    welcome += ADMIN_MSG;
+  ctx.reply(welcome);
 });
-bot.on('text', ctx => {
-  /*pool.query('SELECT table_schema,table_name FROM information_schema.tables LIMIT 3', (err, res) => {
-    if (err) throw err;
-    ctx.reply(JSON.stringify(res.rows));
-  });*/
+
+bot.command('link', ctx => {
   console.log(util.inspect(ctx.update, false, 10));
+  const destinationChatId = ctx.update.message.chat.id;
+  pairingChatIds.push(destinationChatId);
+  ctx.reply(LINK_MSG + destinationChatId);
+});
+
+bot.command('unlink', ctx => {
+  console.log(util.inspect(ctx.update, false, 10));
+  const chatId = ctx.update.message.chat.id;
+  await pool.query('DELETE FROM directions WHERE sourceChatId = $1 OR destinationChatId = $1', [chatId]);
+  ctx.reply('Я більше не пересилатиму з цього чату / у цей чат.');
+});
+
+bot.on('text', async ctx => {
+  console.log(util.inspect(ctx.update, false, 10));
+  const sourceChatId = ctx.update.message.chat.id;
+
+  if(ctx.update.message.text.startsWith(LINK_MSG)) {
+    const destinationChatId = parseInt(ctx.update.message.text.slice(LINK_MSG.length));
+    if(pairingChatIds.includes(destinationChatId)) {
+      pairingChatIds.splice(pairingChatIds.indexOf(destinationChatId), 1);
+      await pool.query('INSERT INTO directions VALUES ($1, $2)', [sourceChatId, destinationChatId]);
+      ctx.reply('Чудово! Відтепер я пересилатиму твої тік-токи до іншого чату.')
+    }
+  } else {
+    const dbResult = await pool.query('SELECT destinationChatId FROM directions WHERE sourceChatId = $1', [sourceChatId]);
+    if(dbResult.rows.length) {
+      const { destinationChatId } = dbResult.rows[0];
+      bot.telegram.sendMessage(destinationChatId, ctx.update.message.text);
+    }
+  }
 });
 bot.launch();
 
