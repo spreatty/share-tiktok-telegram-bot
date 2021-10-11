1db212f3b1fb8a613bd942845cff912ae73a4a7a
Anton Onipko <antono@starsgroup.com>
2021-10-11 18:11:24 +0300

    debug stuff

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-
 const path = '/' + encodeURIComponent(process.env.BOT_TOKEN);
 
 bot.telegram.setWebhook('https://share-tiktok-telegram-bot.herokuapp.com' + path);
-
 bot.startWebhook(path, null, process.env.PORT || 8080);
 
-bot.start(ctx => ctx.reply('Welcome'))
+bot.start(ctx => {
+  console.log(ctx);
+  ctx.reply('Welcome');
+});
 bot.launch();
 
 // Enable graceful stop
