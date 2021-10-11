6edc90f341e0f1e55e2d9d4d8948b199e1e574a5
Anton Onipko <antono@starsgroup.com>
2021-10-11 18:58:57 +0300

    db init

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Telegraf } = require('telegraf');
+const { Client } = require('pg');
 const util = require('util');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
@@ -10,6 +11,22 @@ bot.startWebhook(path, null, process.env.PORT || 8080);
 bot.start(ctx => {
   console.log(util.inspect(ctx.update, false, 10));
   ctx.reply('Welcome');
+});
+bot.hears('give', ctx => {
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+
+  client.connect();
+
+  client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+    if (err) throw err;
+    ctx.reply(JSON.stringify(res.rows));
+    client.end();
+  });
 });
 bot.launch();
 
