15813ac5a81ea156a22d41e87cdcb9c2c93be2e3
Anton Onipko <antono@starsgroup.com>
2021-10-18 15:38:42 +0300

    DB rows as array fix

--- UserBot.js
+++ UserBot.js
@@ -92,7 +92,7 @@ async function onLink(ctx) {
 
 async function list(ctx) {
   const chatId = ctx.update.message.chat.id.toString();
-  const rows = db.getRelatedLinks(chatId);
+  const rows = Array.from(db.getRelatedLinks(chatId));
   const promises = rows.map(({ source, target }) => {
     const type = source == target ? 'loop' : chatId == source ? 'from' : 'to';
     const data = { type };
