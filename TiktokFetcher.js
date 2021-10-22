c3de2727d68031e2c77fc91e8e75660d4378aa51
Anton Onipko <antono@starsgroup.com>
2021-10-22 23:29:06 +0300

    Narrowed http2 hosts

--- TiktokFetcher.js
+++ TiktokFetcher.js
@@ -2,7 +2,10 @@ const https = require('https');
 const http2 = require('http2');
 const EventEmitter = require('events');
 
-const http2Hosts = ['m.tiktok.com', 'www.tiktok.com'];
+const http2Hosts = [
+  //'m.tiktok.com',
+  'www.tiktok.com'
+];
 const commonHeaders = {
   'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15',
   Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
