fbc07073154124ad1ea4e3173f61d936f25e2494
Anton Onipko <antono@starsgroup.com>
2022-07-02 15:28:31 +0300

    Fix

--- Tiktok.js
+++ Tiktok.js
@@ -38,7 +38,7 @@ async function onTiktok({ update }) {
     const fileId = cachedVideo.file_id;
     console.log('Found in database. File id: ' + fileId);
     if(cachedVideo.slides) {
-      targets.forEach(target => bot.telegram.sendMediaGroup(target, cachedVideo.slides.map(slide => ({media: slide})), extra));
+      targets.forEach(target => bot.telegram.sendMediaGroup(target, cachedVideo.slides.map(toPhoto), extra));
     } else {
       extra.width = cachedVideo.width;
       extra.height = cachedVideo.height;
@@ -72,11 +72,11 @@ async function sendAndSaveVideo([ first, ...rest ], videoStream, extra, urls) {
 }
 
 async function sendAndSaveSlides([ first, ...rest ], slideStreams, extra, urls) {
-  const response = await bot.telegram.sendMediaGroup(first, slideStreams.map(stream => ({media: {source: stream}})), extra);
+  const response = await bot.telegram.sendMediaGroup(first, slideStreams.map(streamToPhoto), extra);
   const fileId = response.photo[0].file_id;
   const files = response.photo.map(photo => photo.file_id);
   db.putVideo(fileId, files.join('||'));
-  rest.forEach(target => bot.telegram.sendMediaGroup(target, files.map(file => ({media: file})), extra));
+  rest.forEach(target => bot.telegram.sendMediaGroup(target, files.map(toPhoto), extra));
   urls.forEach(url => db.putUrlRecord(url.toString(), fileId));
 }
 
@@ -88,4 +88,15 @@ async function sendDocument([ first, ...rest ], originalText, docData) {
     await bot.telegram.sendMessage(target, originalText, props.noPreview);
     bot.telegram.sendDocument(target, fileId);
   });
+}
+
+function streamToPhoto(stream) {
+  return toPhoto({source: stream});
+}
+
+function toPhoto(media) {
+  return {
+    type: 'photo',
+    media
+  };
 }
