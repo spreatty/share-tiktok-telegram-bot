c76aa70e3fff36574fb550ab6bd5077842a5ad48
Anton Onipko <antono@starsgroup.com>
2022-07-03 15:00:32 +0300

    Fix

--- TiktokFetcher.js
+++ TiktokFetcher.js
@@ -9,7 +9,6 @@ const http2Hosts = [
 ];
 const commonHeaders = {
   'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Safari/605.1.15',
-  'User-Agent': 'Mozilla/5.0 (Linux; Android 10; SM-G981B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Mobile Safari/537.36',
   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
   'Accept-Language': 'en-gb',
   'Accept-Encoding': 'identity'
