283b3bc8b188051683a4ecc6fe9df23e86622641
Anton Onipko <antono@starsgroup.com>
2022-11-15 12:35:08 +0200

    Improvements

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
+const fastify = require('fastify');
 const { Telegraf } = require('telegraf');
 const { isTiktokUrl, onTiktok } = require('./Tiktok');
 const UserBot = require('./UserBot');
 const AdminBot = require('./AdminBot');
 const db = require('./db');
 
-db.connect();
-db.createSchema();
-
+const app = fastify();
 const bot = global.bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.use((ctx, next) => {
-  console.log(JSON.stringify(ctx.update));
-  next();
+const domain = process.env.PROJECT_DOMAIN ? process.env.PROJECT_DOMAIN + '.glitch.me' : 'completely-wrong-domain.glitch.me';
+const port = process.env.PORT || 8080;
+const path = '/telegraf/' + bot.secretPathComponent();
+
+app.get('/', (req, res) => {
+  res.code(200).send("It's a bot");
 });
 
-process.once('SIGINT', () => {
-  bot.stop('SIGINT');
+process.on('breforeExit', () => {
+  console.log('Exiting');
   db.close();
-});
-process.once('SIGTERM', () => {
-  bot.stop('SIGTERM');
-  db.close();
+  process.exit(0);
 });
 
 UserBot.addHandlers();
@@ -28,9 +27,10 @@ AdminBot.addHandlers();
 
 bot.url(isTiktokUrl, onTiktok);
 
-bot.launch({
-  webhook: {
-    domain: 'share-tiktok-bot.glitch.me',
-    port: process.env.PORT
-  }
-});
+const webhook = bot.webhookCallback(path);
+app.all(path, (req, res) => webhook(req.raw, res.raw));
+
+db.connect()
+  .then(db.createSchema)
+  .then(() => app.listen({ port }, () => console.log(`Serving https://${domain}${path} on ${port}`)))
+  .then(() => bot.telegram.setWebhook('https://' + domain + path));
