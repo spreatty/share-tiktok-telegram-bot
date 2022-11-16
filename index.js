0c8031c5754bd0ce183398b347d1c750e96dea0e
Anton Onipko <antono@starsgroup.com>
2022-11-16 21:02:17 +0200

    Upgrade

--- index.js
+++ index.js
@@ -5,31 +5,44 @@ const UserBot = require('./UserBot');
 const AdminBot = require('./AdminBot');
 const db = require('./db');
 
-const app = express();
 const bot = global.bot = new Telegraf(process.env.SHARE_TIKTOK_BOT_TOKEN);
+const path = '/telegraf/' + bot.secretPathComponent();
+const webhookUrl = 'https://' + process.env.SHARE_TIKTOK_BOT_DOMAIN + path;
 
 bot.use((ctx, next) => {
   console.log(JSON.stringify(ctx.update));
   next();
 });
 
-process.once('SIGINT', () => {
-  bot.stop('SIGINT');
-  db.close();
-});
-process.once('SIGTERM', () => {
-  bot.stop('SIGTERM');
-  db.close();
-});
-
 UserBot.addHandlers();
 AdminBot.addHandlers();
-
 bot.url(isTiktokUrl, onTiktok);
 
-bot.launch({
-  webhook: {
-    domain: process.env.SHARE_TIKTOK_BOT_WEBHOOK,
-    port: process.env.PORT
-  }
+db.init(process.env.SHARE_TIKTOK_BOT_DB_CONN);
+
+const app = express();
+const webhookCallback = bot.webhookCallback(path);
+
+app.get('/', (req, res) => {
+  res.send({ status: 'ok' });
 });
+
+app.post(path, (req, res) => {
+  console.log('Webhook request');
+  webhookCallback(req, res);
+});
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Ready. Share TikTok Bot serves at ${webhookUrl}`);
+  bot.telegram.setWebhook(webhookUrl).then(() => console.log('Webhook set'));
+});
+
+const exit = () => {
+  Promise.all(new Promise(resolve => server.close(resolve)), db.close())
+    .then(() => server.closeAllConnections())
+    .then(() => process.exit(0));
+  setTimeout(() => process.exit(0), 1000).unref();
+};
+
+process.once('SIGINT', exit);
+process.once('SIGTERM', exit);
